36f513c43ef6895cf6ff5902deef164a2b91c03b
MaryHooker <mc020791@yahoo.com>
2020-04-03 22:44:53 -0500

    styling

--- login-api-app/src/components/AppContainer.jsx
+++ login-api-app/src/components/AppContainer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Animated } from "react-animated-css";
+import { Button } from 'react-bootstrap';
 
 
 class AppContainer extends Component {
@@ -7,12 +8,36 @@ class AppContainer extends Component {
         super(props);
         this.state = {}
     }
+
     render() {
+
         return (
+            
             <div>
+
                 <Animated animationIn="fadeInUp" animationInDuration={1000} animationOutDuration={1000} isVisible={true}>
-                    <p className='welcomeSign'>Welcome</p>
+
+                    <div className='appContainer'>
+
+                    <div className='title'>
+                        <p className='welcomeSign'>Welcome</p>
+                    </div>
+
+
+                    <div className='login'>
+                        <Button variant='success' className='buttons'>Login</Button>
+                    </div>
+
+                    <br />
+
+                    <div className='signUp'>
+                        <Button name='button' variant='success' className='buttons'>Sign Up</Button>
+                    </div>
+
+                    </div>
+
                 </Animated>
+
             </div>
         );
     }
